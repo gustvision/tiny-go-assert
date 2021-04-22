f0ad8502df6a1bb3bb44a031004cd030c06221a2
Karine Valenca <valenca.karine@gmail.com>
2021-04-22 11:39:37 -0300

    improve error return

--- assert/assertion_compare.go
+++ assert/assertion_compare.go
@@ -2,30 +2,30 @@ package assert
 
 import "fmt"
 
-func Equal(e1 interface{}, e2 interface{}) error {
-	if e1 == e2 {
+func Equal(expected interface{}, actual interface{}) error {
+	if expected == actual {
 		return nil
 	}
-	return fmt.Errorf(`Test failed: Expected "%s" to be equals to "%s, but got %s"`, e1, e2, e1)
+	return fmt.Errorf(`Test failed: Expected: %s	Actual: %s`, expected, actual)
 }
 
-func Nil(e1 interface{}) error {
-	if isNil(e1) {
+func Nil(actual interface{}) error {
+	if isNil(actual) {
 		return nil
 	}
-	return fmt.Errorf(`Test failed: Expected "%s" to be nil, but got %s"`, e1, e1)
+	return fmt.Errorf(`Test failed: Expected to be nil but got %s"`, actual)
 }
 
-func NotNil(e1 interface{}) error {
-	if isNil(e1) {
-		return fmt.Errorf(`Test failed: Expected "%s" to be nil, but got %s"`, e1, e1)
+func NotNil(actual interface{}) error {
+	if isNil(actual) {
+		return fmt.Errorf(`Test failed: Expected to be not nil, but got %s"`, actual)
 	}
 	return nil
 }
 
-func Empty(e1 []interface{}) error {
-	if len(e1) == 0 {
+func Empty(actual []interface{}) error {
+	if len(actual) == 0 {
 		return nil
 	}
-	return fmt.Errorf(`Test failed: Expected "%s" to be empty, but got %s"`, e1, e1)
+	return fmt.Errorf(`Test failed: Expected to be empty, but got %s"`, actual)
 }
